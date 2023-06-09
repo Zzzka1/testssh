71a71a300be966ce73edbf43660afa99bc1a1483
zhangkai <1322508457@qq.com>
2023-06-09 21:28:12 +0800

    test ssh

---
+++ src/main/java/org/atguigu/Test.java
@@ -0,0 +1,12 @@
+package org.atguigu;
+
+/**
+ * @Author Zzzka1
+ * @Date 2023/6/9 21:26
+ * @Version 1.0
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Test");
+    }
+}
